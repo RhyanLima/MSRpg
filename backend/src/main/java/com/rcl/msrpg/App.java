b4bf1df98b66eb0dbc719ea642c380ce18881d05
rhyan.lima <rhyanlima.dev@gmail.com>
2026-06-05 20:59:04 -0300

    add: configuração do pom.xml

---
+++ backend/src/main/java/com/rcl/msrpg/App.java
@@ -0,0 +1,13 @@
+package com.rcl.msrpg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
