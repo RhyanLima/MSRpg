98a88eebebeceb9b198c46b287cb47500b31c89c
rhyan.lima <rhyanlima.dev@gmail.com>
2026-06-12 13:44:05 -0300

    wip: etidade de dominio RpgSystem

---
+++ backend/src/main/java/com/rcl/msrpg/system/domain/model/RpgSystem.java
@@ -0,0 +1,15 @@
+package com.rcl.msrpg.system.domain.model;
+
+import com.rcl.msrpg.shared.identifier.RpgSystemId;
+
+public class RpgSystem {
+
+    private RpgSystemId id;
+    private String name;
+    private String description;
+    private String engineVersion;
+    private String contentVersion;
+    // private ResolutionPolicy defaultResolutionPolicy;
+    private String defaultResolutionPolicy;
+
+}
